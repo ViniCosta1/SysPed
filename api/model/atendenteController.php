65c0581fe85a03fc8a50debcb93ac78eea0775a7
ViniCosta1 <vinicosta154@hotmail.com>
2023-05-19 12:00:13 -0300

    Atendente e Venda

---
+++ api/model/atendenteController.php
@@ -0,0 +1,135 @@
+<?php
+
+include ("../connection/conn.php");
+
+$requisicao = $_REQUEST;
+
+if ($requisicao['operation'] == 'create') {
+    try {
+        // Gerar a querie de inserção de dados no B.D.
+        $sql = "INSERT INTO ATENDENTE (NOME, LOGIN, SENHA, ACESSO_ID) VALUES (?, ?, ?, ?)";
+        // Iremos preparar a nossa querie para gerar o objeto de inserção ao B.D.
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            $requisicao['NOME'],
+            $requisicao['LOGIN'],
+            $requisicao['SENHA'],
+            $requisicao['ACESSO_ID']
+        ]);
+        $dados = array(
+            'type' => 'success',
+            'mensagem' => 'Registro salvo com sucesso!'
+        );
+    } catch (PDOException $e) {
+        $dados = array(
+            'type' => 'error',
+            'message' => 'Erro ao salvar o registro: '.$e
+        );
+    }
+
+    echo json_encode($dados);
+}
+
+if ($requisicao['operation'] == 'read') {
+    // Obter o númeor de colunas da nossa tabela
+    $colunas = $requisicao['columns'];
+
+    // Gerar a nossa query de consulta ao banco de dados
+    $sql = "SELECT * FROM ATENDENTE WHERE 1=1";
+
+    // Obter o total de registros encontrados
+    $resultados = $pdo->query($sql);
+
+    // Contar quantos registros tem nesse objeto
+    $qtdLinhas = $resultado->rowCount();
+
+    // Verificar se existe algum filtro determinado
+    $filtro = $requisicao['search']['value'];
+    if (!empty($filtro)) {
+        $sql .= " AND (ID LIKE $filtro% ";
+        $sql .= " OR NOME LIKE %$filtro) ";
+    }
+
+    // Obter o total de registros encontrados filtrados
+    $resultados = $pdo->query($sql);
+
+    // Contar quantos registros tem nesse objeto filtrado
+    $totalFiltrados = $resultado->rowCount();
+
+    // Obter os valores para ordenação de registro
+    $colunaOrdem = $requisicao['ordem'][0]['column']; // obtendo a posição da coluna na ordenação
+    $ordem = $colunas[$colunaOrdem]['data']; // obtendo o nome da coluna que será ordenada
+    $direcao = $requisicao['order'][0]['dir']; // obtendo a direção da ordenação ASC | DESC
+
+    // Obter os limites para a paginação dos dados
+    $inicio = $requisicao['start']; // obtendo o inicio do limite
+    $tamanho = $requisicao['length']; // obtendo o tamanho do limite
+
+    // Realizar a nossa ordenação e os limites
+    $sql .= " ORDER BY $ordem $direcao LIMIT $inicio $tamanho ";
+    $resultado = $pdo->query($sql);
+    $dados = array();
+    while ($row = $resultado->fetch(PDO::FETCH_ASSOC)) {
+        $dados[] = array_map(null, $row);
+    }
+
+    // Montar o objeto JSON no padrão DataTables
+    $json_data = array(
+        "draw" => intval($requisicao['draw']),
+        "recordsTotal" => intval($qtdLinhas),
+        "recordsFiltered" => intval($totalFiltrados),
+        "data" => $dados
+    );
+
+    echo json_encode($json_data);
+}
+
+if ($requisicao['operation'] == 'update') {
+    try {
+        // Gerar a querie de inserção de dados no B.D.
+        $sql = "UPDATE ATENDENTE SET NOME = ?, LOGIN = ?, SENHA = ?, ACESSO_ID = ? WHERE ID = ?";
+        // Iremos preparar a nossa querie para gerar o objeto de inserção ao B.D.
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            $requisicao['NOME'],
+            $requisicao['LOGIN'],
+            $requisicao['SENHA'],
+            $requisicao['ACESSO_ID'],
+            $requisicao['ID']
+        ]);
+        $dados = array(
+            'type' => 'success',
+            'mensagem' => 'Registro atualizado com sucesso!'
+        );
+    } catch (PDOException $e) {
+        $dados = array(
+            'type' => 'error',
+            'mensagem' => 'Erro ao atualizar o registro: '.$e
+        );
+    }
+
+    echo json_encode($dados);
+}
+
+if ($requisicao['operation'] == 'delete') {
+    try {
+        // Gerar a querie de inserção de dados no B.D.
+        $sql = "DELETE FROM ATENDENTE WHERE ID = ?";
+        // Iremos preparar a nossa querie para gerar o objeto de inserção ao B.D.
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            $requisicao['ID']
+        ]);
+        $dados = array(
+            'type' => 'success',
+            'mensagem' => 'Registro excluído com sucesso!'
+        );
+    } catch (PDOException $e) {
+        $dados = array(
+            'type' => 'error',
+            'mensagem' => 'Erro ao excluir o registro: '.$e
+        );
+    }
+
+    echo json_encode($dados);
+}
